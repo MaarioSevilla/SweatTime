2fe5d5c1563abb2de583fc951f02d15e7335a7a7
sevilla <oficialmariosevilla@icloud.com>
2020-02-24 18:25:27 -0600

    commit dont working

--- app/src/main/java/com/sevillaInc/sweattime/HomeFragment.java
+++ app/src/main/java/com/sevillaInc/sweattime/HomeFragment.java
@@ -8,6 +8,10 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
+import java.util.logging.SimpleFormatter;
 
 
 /**
@@ -15,6 +19,33 @@ import android.view.ViewGroup;
  */
 public class HomeFragment extends Fragment {
 
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        /*Thread t = new Thread() {
+            @Override
+            public void run() {
+                try {
+                    while (!isInterrupted()) {
+                        Thread.sleep(1000);
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                TextView tdate = (TextView)findViewById(R.id.date);
+                                long date = System.currentTimeMillis();
+                                SimpleDateFormat sdf = new SimpleDateFormat("MMM dd yyyy\nhh-mm-ss a");
+                                String dateString = sdf.format(date);
+                                tdate.setText(dateString);
+                            }
+                        });
+                    }
+                } catch (InterruptedException e) {
+                }
+            }
+        };
+        t.start();*/
+    }
 
     public HomeFragment() {
         // Required empty public constructor
@@ -27,5 +58,4 @@ public class HomeFragment extends Fragment {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false);
     }
-
 }
