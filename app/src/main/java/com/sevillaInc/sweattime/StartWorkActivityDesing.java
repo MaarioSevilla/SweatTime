a10007f2bd17722f9e8673d863d09a6c8dd8e7d3
sevilla <oficialmariosevilla@icloud.com>
2020-02-29 17:36:13 -0600

    commit firefase y multilenguaje en implementacion

---
+++ app/src/main/java/com/sevillaInc/sweattime/StartWorkActivityDesing.java
@@ -0,0 +1,14 @@
+package com.sevillaInc.sweattime;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class StartWorkActivityDesing extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start_work_desing);
+    }
+}
